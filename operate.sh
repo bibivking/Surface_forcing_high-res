f9406ff991d167ccd6383ef1b24465e97c334c95
Mengyuan Mu <bibi_king@163.com>
2020-10-28 16:21:45 +1100

    to use new iveg

--- operate.sh
+++ operate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd /srv/ccrc/data25/z5218916/script/Surface_forcing_high-res
-ncl form_gridinfo_AWAP_first-step.ncl
-ncl form_gridinfo_AWAP_second-step.ncl
+nohup ncl form_gridinfo_AWAP_first-step.ncl
+nohup ncl form_gridinfo_AWAP_second-step.ncl
+nohup ncl form_gridinfo_AWAP_third-step.ncl
